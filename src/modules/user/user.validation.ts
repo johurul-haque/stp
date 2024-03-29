e0e0c92ef34f455487798f0e9b06aefd58c0738e
Johurul Haque <johurulhaquejony@gmail.com>
2024-03-29 23:11:34 +0600

    code refactoring

--- src/modules/user/user.validation.ts
+++ src/modules/user/user.validation.ts
@@ -1,20 +1,22 @@
 import { hashPassword } from '@/utils/hash-password';
 import { z } from 'zod';
 
+function requiredError(fieldName: string) {
+  return { required_error: `${fieldName} field is required` };
+}
+
 export const registerPayload = z.object({
-  name: z.string({ required_error: 'Name field is required.' }),
+  name: z.string(requiredError('Name')),
   email: z
     .string({ required_error: 'Email must be a valid email address.' })
     .email({ message: 'Email must be a valid email address.' }),
-  password: z
-    .string({ required_error: 'Password field is required.' })
-    .transform(hashPassword),
+  password: z.string(requiredError('Password')).transform(hashPassword),
   profile: z.object(
     {
-      bio: z.string({ required_error: 'Bio field is required.' }),
-      age: z.number({ required_error: 'Age field is required.' }),
+      bio: z.string(requiredError('Bio')),
+      age: z.number(requiredError('Age')),
     },
-    { required_error: 'Profile field is required.' }
+    requiredError('Profile')
   ),
 });
 
@@ -22,7 +24,7 @@ export const loginPayload = z.object({
   email: z
     .string({ required_error: 'Email must be a valid email address.' })
     .email({ message: 'Email must be a valid email address.' }),
-  password: z.string({ required_error: 'Password field is required.' }),
+  password: z.string(requiredError('Password')),
 });
 
 export const jwtPayload = z.object({
