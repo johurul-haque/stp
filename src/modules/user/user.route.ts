193a0f308135f7711f4bdc699134243e2283dd18
Johurul Haque <johurulhaquejony@gmail.com>
2024-03-29 16:40:17 +0600

    validating incoming payload and changed required error messages

--- src/modules/user/user.route.ts
+++ src/modules/user/user.route.ts
@@ -1,8 +1,10 @@
+import { validateRequest } from '@/middlewares/validate-request';
 import { Router } from 'express';
 import { registerUser } from './user.controller';
+import { registerPayload } from './user.validation';
 
 const router = Router();
 
-router.post('/register', registerUser);
+router.post('/register', validateRequest(registerPayload), registerUser);
 
 export const UserRoutes = router;
--- src/modules/user/user.validation.ts
+++ src/modules/user/user.validation.ts
@@ -3,12 +3,17 @@ import { z } from 'zod';
 
 export const registerPayload = z.object({
   name: z.string({ required_error: 'Name field is required.' }),
-  email: z.string().email({ message: 'Email must be a valid email address.' }),
+  email: z
+    .string({ required_error: 'Email must be a valid email address.' })
+    .email({ message: 'Email must be a valid email address.' }),
   password: z
     .string({ required_error: 'Password field is required.' })
     .transform(hashPassword),
-  profile: z.object({
-    bio: z.string({ required_error: 'Bio field is required.' }),
-    age: z.number({ required_error: 'Age field is required.' }),
-  }),
+  profile: z.object(
+    {
+      bio: z.string({ required_error: 'Bio field is required.' }),
+      age: z.number({ required_error: 'Age field is required.' }),
+    },
+    { required_error: 'Profile field is required.' }
+  ),
 });
