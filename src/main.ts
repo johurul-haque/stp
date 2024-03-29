1a864297f24e472f54ae71a03fd602989df58bc5
Johurul Haque <johurulhaquejony@gmail.com>
2024-03-29 12:37:59 +0600

    added model schema

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { env } from '@config';
 import app from 'app';
+import { db } from './config/db';
 
 (async function () {
   try {
+    await db.$connect();
+
     app.listen(env.PORT, () => {
       console.log(`Server listening on ${env.PORT}`);
     });
