4f27f79daeb980f2012617a688543a787c8dfbe0
Johurul Haque <johurulhaquejony@gmail.com>
2024-03-29 17:52:22 +0600

    added verifyToken middleware for decoding jsonwebtoken

---
+++ src/middlewares/verify-token.ts
@@ -0,0 +1,20 @@
+import { env } from '@/config';
+import { jwtPayload } from '@/modules/user/user.validation';
+import { AppError, catchAsync } from '@/utils';
+import jwt from 'jsonwebtoken';
+
+export const verifyToken = catchAsync((req, _, next) => {
+  try {
+    const bearerToken = req.headers.authorization;
+
+    if (!bearerToken) throw new Error();
+
+    const decoded = jwt.verify(bearerToken, env.JWT_SECRET);
+
+    req.jwtPayload = jwtPayload.parse(decoded);
+
+    return next();
+  } catch (error) {
+    throw new AppError(401, 'Unauthorized Access');
+  }
+});
