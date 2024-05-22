2e46812df619a33dce491587e6a7195d5854e644
Johurul Haque <johurulhaquejony@gmail.com>
2024-05-22 16:34:38 +0600

    added middleware and rewrites

---
+++ src/middleware.ts
@@ -0,0 +1,33 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function middleware(request: NextRequest) {
+  const cookie = request.cookies.get('access_token');
+  const path = request.nextUrl.pathname;
+
+  const authRoutes = ['/login', '/register'];
+
+  if (!cookie && !authRoutes.includes(path)) {
+    return NextResponse.redirect(new URL('/login', request.url));
+  }
+
+  if (!cookie) return;
+
+  const url = `${process.env.BASE_API_URL}/users/profile`;
+  const res = await fetch(url, {
+    headers: {
+      Authorization: cookie.value,
+    },
+  });
+
+  if (res.status !== 200 && !authRoutes.includes(path)) {
+    return NextResponse.redirect(new URL('/login', request.url));
+  }
+
+  if (!path.startsWith('/dashboard')) {
+    return NextResponse.redirect(new URL('/dashboard', request.url));
+  }
+}
+
+export const config = {
+  matcher: ['/dashboard/:path*', '/login', '/register'],
+};
