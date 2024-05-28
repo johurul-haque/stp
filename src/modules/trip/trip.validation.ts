f1518b587ad7e5b2e8f7f28b6c152b5f86737c13
Johurul Haque <johurulhaquejony@gmail.com>
2024-05-28 18:41:45 +0600

    transforming datetime to UTC string before saving to db

--- src/modules/trip/trip.validation.ts
+++ src/modules/trip/trip.validation.ts
@@ -5,17 +5,24 @@ function requiredError(fieldName: string) {
 }
 
 export const createTripPayload = z.object({
-  destination: z.string(requiredError('Destination')),
+  destination: z.string(requiredError('destination')),
   description: z
     .string()
     .min(40, 'Description must be at least 40 characters.')
     .max(400, 'Description must be at in-between 400 characters.'),
   images: z.string().url().array(),
   travelType: z.string(),
-  startDate: z.string(requiredError('StartDate')),
-  endDate: z.string(requiredError('EndDate')),
+  startDate: z
+    .string(requiredError('startDate'))
+    .datetime()
+    .transform(toUTCString),
+  endDate: z.string(requiredError('endDate')).datetime().transform(toUTCString),
 });
 
 export const tripPairRequestPayload = z.object({
   userId: z.string().uuid(),
 });
+
+function toUTCString(value: string) {
+  return new Date(value).toUTCString();
+}
