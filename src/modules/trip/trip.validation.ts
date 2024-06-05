113c11ed32a0e432dcf45a9d4c6b179d287651a0
Johurul Haque <johurulhaquejony@gmail.com>
2024-06-05 13:21:20 +0600

    refactored code

--- src/modules/trip/trip.validation.ts
+++ src/modules/trip/trip.validation.ts
@@ -8,8 +8,7 @@ export const createTripPayload = z.object({
   destination: z.string(requiredError('destination')),
   description: z
     .string()
-    .min(40, 'Description must be at least 40 characters.')
-    .max(400, 'Description must be at in-between 400 characters.'),
+    .min(40, 'Description must be at least 40 characters.'),
   images: z.string().url().array(),
   travelType: z.string(),
   startDate: z
