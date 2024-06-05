75824de04cb8fe11867d5bca476e5503a9161bc3
Johurul Haque <johurulhaquejony@gmail.com>
2024-06-05 21:19:38 +0600

    removed unnecessary type

--- src/modules/trip/trip.interface.ts
+++ src/modules/trip/trip.interface.ts
@@ -1,5 +1,4 @@
 import { z } from 'zod';
-import { createTripPayload, tripPairRequestPayload } from './trip.validation';
+import { createTripPayload } from './trip.validation';
 
 export type CreateTripPayload = z.infer<typeof createTripPayload>;
-export type TripPairRequestPayload = z.infer<typeof tripPairRequestPayload>;
